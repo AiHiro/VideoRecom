701ae40b60d4b4916788ea60784f940f15f10d4a
AiHiro <13741363+AiHiro@users.noreply.github.com>
2019-07-16 22:12:09 +0900

    add the button _maedasatoshi

--- app/src/main/java/com/example/videorecomapp/MainActivity.java
+++ app/src/main/java/com/example/videorecomapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.videorecomapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        test2
+//        add the button
+
+        final Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.d("debug", "button1, Perform action on click");
+            }
+        });
     }
 }
